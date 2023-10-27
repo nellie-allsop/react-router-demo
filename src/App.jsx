da6061ef42ea43d726d1e095f2256b10407553db
Nellie Allsop <nellie.allsop@gmail.com>
2023-10-27 11:35:13 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import "./App.css"
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>React Router Dom</h1>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </header>
+
+<Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<About />} />
+</Routes>
+
+      <footer>
+        <p>&copy; Tim the Bin</p>
+      </footer>
+      </BrowserRouter>
+  )
+}
+
+export default App
